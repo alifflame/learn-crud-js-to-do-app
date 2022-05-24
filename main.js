441ce99466492a0e23b419cfd9e2f75b87f39392
Alif Aunurrohim <alifaunurrohim@gmail.com>
2022-05-25 01:56:34 +0700

    add js crud mainfiles

---
+++ main.js
@@ -0,0 +1,13 @@
+let form = document.getElementById("form");
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+});
+
+let formValidation = () => {
+    if () {
+        console.log('failure');
+    } else {
+        console.log('success');
+    }
+};
